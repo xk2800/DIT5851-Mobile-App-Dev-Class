0d8fffa0522129eae7425b3ee54a55f1d797592b
Xavier K <xavierkhew00@gmail.com>
2020-12-21 15:23:33 +0800

    Upload all files

---
+++ LAB4/LAB4-1/app/src/main/java/com/ebookfrenzy/eventexample/MainActivity.java
@@ -0,0 +1,110 @@
+package com.ebookfrenzy.eventexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import androidx.core.view.GestureDetectorCompat;
+
+
+public class MainActivity extends AppCompatActivity
+    implements GestureDetector.OnGestureListener,
+    GestureDetector.OnDoubleTapListener
+{
+    private TextView gestureText;
+    private GestureDetectorCompat gDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gestureText = findViewById(R.id.statusText); //for gesture
+        this.gDetector = new GestureDetectorCompat(this,this);
+        gDetector.setOnDoubleTapListener(this);
+
+
+        Button button = findViewById(R.id.myButton); //grab button's ID listed in activity_main.xml
+        button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) { //single short click
+                        TextView statusText = findViewById(R.id.statusText);
+                        statusText.setText("Button clicked"); //message shown when button is clicked
+                    }
+                }
+        );
+
+        button.setOnLongClickListener(
+                new Button.OnLongClickListener() {
+                    public boolean onLongClick(View v) { //longclick
+                        TextView statusText =
+                                findViewById(R.id.statusText);
+                        statusText.setText("Long button click");
+                        return false; //false = when button release, msg gone, true = when button release, msg still there
+                    }
+                }
+        );
+
+    }
+//all the gestures
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        gestureText.setText("onFling");
+        return true;
+    }
+    @Override
+    public void onLongPress(MotionEvent event) {
+        gestureText.setText("onLongPress");
+    }
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                            float distanceX, float distanceY) {
+        gestureText.setText("onScroll");
+        return true;
+    }
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        gestureText.setText("onSingleTapConfirmed");
+        return true;
+    }
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        gestureText.setText("onDoubleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        gestureText.setText ("onDown");
+        return true;
+    }
+    @Override
+    public void onShowPress(MotionEvent event) {
+        gestureText.setText("onShowPress");
+    }
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        gestureText.setText("onDoubleTapEvent");
+        return true;
+    }
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        gestureText.setText("onSingleTapUp");
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
+}
