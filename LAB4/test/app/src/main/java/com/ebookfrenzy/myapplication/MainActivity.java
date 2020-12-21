35698800d159165c70ed70270019c36c02f3e107
Xavier K <xavierkhew00@gmail.com>
2020-12-21 15:43:54 +0800

    2

---
+++ LAB4/test/app/src/main/java/com/ebookfrenzy/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ebookfrenzy.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//test main project
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
