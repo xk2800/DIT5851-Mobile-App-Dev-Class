0478dfbd4e95aa7d09ea6fce75e3307fabbddd9f
Xavier K <xavierkhew00@gmail.com>
2021-01-21 17:33:35 +0800

    123

--- LAB8/LAB8-1/app/src/main/java/com/ebookfrenzy/roomdemo/Product.java
+++ LAB8/LAB8-1/app/src/main/java/com/ebookfrenzy/roomdemo/Product.java
@@ -13,11 +13,14 @@ public class Product {
     @ColumnInfo(name = "productName")
     private String name;
     private int quantity;
-    public Product(String name, int quantity) {
+    private int price;
+    public Product(String name, int quantity, int price) {
         this.id = id;
         this.name = name;
         this.quantity = quantity;
+        this.price = price;
     }
+
     public int getId() {
         return this.id;
     }
@@ -27,13 +30,13 @@ public class Product {
     public int getQuantity() {
         return this.quantity;
     }
+    public int getPrice() {
+        return this.price;
+    }
     public void setId(int id) {
         this.id = id;
     }
     public void setName(String name) {
         this.name = name;
-    }
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 }
