60adfcd34102833227e04c0bc9d8aeff5f7bfc68
Xavier K <xavierkhew00@gmail.com>
2021-01-25 15:03:37 +0800

    LAB 9 EXAMPLE 1

---
+++ LAB9/LAB9-1-1/app/src/main/java/com/ebookfrenzy/alarmapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ebookfrenzy.alarmapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startAlert(View view) {
+        EditText text = (EditText) findViewById(R.id.time);
+        int i = Integer.parseInt(text.getText().toString());
+        Intent intent = new Intent(this, MyReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        AlarmManager alarmManager = (AlarmManager)
+                getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
+                + (i * 1000), pendingIntent);
+        Toast.makeText(this, "Alarm set in " + i + " seconds",
+                Toast.LENGTH_LONG).show();
+    }
+
+}
